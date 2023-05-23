845957dec7d8eecddfb667292ff69e674d6b0f87
Weronika <67145460+WeraGitHub@users.noreply.github.com>
2023-05-23 20:12:16 +0100

    Update greetings.js

--- Kickstarter_JavaScript/Session1/greetings.js
+++ Kickstarter_JavaScript/Session1/greetings.js
@@ -6,4 +6,17 @@ const greeting = 'Hello';
 const my_name = 'Weronika';
 const emoji = '💕';
 
-console.log(greeting + my_name + emoji)
+// concatonation
+console.log(greeting + " " + my_name + " " + emoji);
+//interpolation
+const message = `${emoji} ${greeting} ${emoji}`;
+console.log(message);
+
+const x = 5;
+const y = 6;
+const peod = x * y;
+console.log(peod);
+
+
+const number = prompt("What's your favourite number?");
+console.log(number);
