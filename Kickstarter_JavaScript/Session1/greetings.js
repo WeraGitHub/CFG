5979e033a62516603aad22101f1648556b7d00ca
Weronika <67145460+WeraGitHub@users.noreply.github.com>
2023-05-23 19:30:52 +0100

    Create greetings.js

---
+++ Kickstarter_JavaScript/Session1/greetings.js
@@ -0,0 +1,9 @@
+// console.log("WEronika");
+// const message = "Weronika 🔥"
+// console.log(message);
+
+const greeting = 'Hello';
+const my_name = 'Weronika';
+const emoji = '💕';
+
+console.log(greeting + my_name + emoji)
